8a26d4198d3d8b801ff1ac780d3f378e9aff04cd
Macicas Teodor <murzo@murzo-laptop.(none)>
2013-02-15 17:23:12 +0100

    some changes for plots data and script

--- ld_in_couch-vs-cumulus_rdf/optimized_couchdb/plot_loading_time.sh
+++ ld_in_couch-vs-cumulus_rdf/optimized_couchdb/plot_loading_time.sh
@@ -8,7 +8,7 @@ fi
 COM
 
 FILE1=loading_time+size_on_disk.log 
-OUTPUT_FILE=loading_time
+OUTPUT_FILE=loading_time_comp
 
 echo "Output file $OUTPUT_FILE"
 gnuplot -persist << EOF 
@@ -16,15 +16,15 @@ set output "${OUTPUT_FILE}.png"
 set term post eps
 set style data histogram
 set style fill solid
-set key top left
+set key top righ
 set term png
 set auto x 
 set ylabel "Loading time (sec) - LOG SCALE" 
 set logscale y
-set xtic rotate by 90 scale 0
+#set xtic rotate by 90 scale 0
 set yrange [1:*]
 set boxwidth 0.8
-plot  "${FILE1}" using 2:xtic(1) title col  linecolor rgb "#0000ff",\
+plot  "${FILE1}" using 8:xtic(1) title col  linecolor rgb "#0000ff",\
       "${FILE1}" using 4:xtic(1) title col  linecolor rgb "#ff4030",\
       "${FILE1}" using 6:xtic(1) title col  linecolor rgb "#00ff00"
 EOF
