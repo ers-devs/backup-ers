4039e2ec70e610d1f1a7607af522f95c8a260ed5
Marat Charlaganov <charlaganov@gmail.com>
2013-05-17 05:40:12 +0200

    rename mesh_replication.py xo_all_to_all.py

---
+++ tests/xo_all_to_all.py
@@ -0,0 +1,21 @@
+import sys, os; sys.path.extend(['/Users/marat/projects/wikireg/site-packages'])
+import couchdbkit
+import socket
+
+hosts = ['xo-0d-58-b2.local.', 'xo-15-4c-93.local.', 'xo-26-7a-e7.local.', 'xo-3c-ea-3a.local.']
+hosts = [socket.gethostbyname(h) for h in hosts]
+
+urls = ['http://admin:admin@' + str(x) + ':5984'
+          for x in hosts]
+
+repl_doc = {'source': 'ers', 'target': 'ers', 'continuous': True, 'create_target': False}
+
+for url in urls:
+    replicator = couchdbkit.Server(url).get_db('_replicator')
+    replicator.server.get_or_create_db('ers')
+    for target_url in urls:
+        doc = repl_doc.copy()
+        doc["target"] = target_url + '/ers'
+        repl_id = url[19:30] + '_to_' + target_url[19:30]
+        doc["_id"] = repl_id
+        print repl_id, replicator.save_doc(doc, force_update=True)['ok']
