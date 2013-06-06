5802c536c5ba7b377e1ba784bd33eb86714f8966
Marat Charlaganov <charlaganov@gmail.com>
2013-06-06 18:04:22 +0200

    fix indentation

--- tests/perf_local_scripts/write_continuously.py
+++ tests/perf_local_scripts/write_continuously.py
@@ -12,15 +12,16 @@ def random_urn():
     return "urn:ers:testdata:{0}".format(random.randint(0, 10e10))
 
 def random_string():
-	[]
+    []
     return "urn:ers:testdata:{0}".format(random.randint(0, 10e10))
 
 
 def loop_write(run_time=1, delay=0):
-	t0 = time()
-	while time() - t0 < run_time:
-		registry.add_data(random_urn(), random_urn(), "test5678910"*1000, node)
-		sleep(delay)
+    t0 = time()
+    while time() - t0 < run_time:
+        registry.add_data(random_urn(), random_urn(), "test5678910"*1000, node)
+        sleep(delay)
 
 if __name__ == '__main__':
-	loop_write(*map(float, sys.argv[1:]))
+    loop_write(*map(float, sys.argv[1:]))
+
