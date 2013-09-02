14906f337f8edf19e8093ad0c1d50d7c50ad8b7d
Marat Charlaganov <charlaganov@gmail.com>
2013-09-02 15:40:09 +0200

    delete db after testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,15 +10,17 @@ from ers.utils import import_nt, import_nt_rdflib
 
 nt_file = os.path.join(TESTS_PATH, 'data', 'timbl.nt')
 
-def test():
-    ers = ERSLocal(dbname='ers_test_utils', reset_database=True)
+def test(keep_db=False):
+    dbname = 'ers_test_utils'
+    ers = ERSLocal(dbname=dbname, reset_database=True)
     import_nt(ers, nt_file, 'timbl')
     assert ers.exist('http://www4.wiwiss.fu-berlin.de/booksMeshup/books/006251587X', 'timbl') == True
     import_nt_rdflib(ers, nt_file, 'timbl2')
     assert ers.exist('http://www4.wiwiss.fu-berlin.de/booksMeshup/books/006251587X', 'timbl2') == True
     assert ers.exist('http://www4.wiwiss.fu-berlin.de/booksMeshup/books/006251587X', 'timbl3') == False
     print "Tests pass"
-
+    if not keep_db:
+        ers.db.server.delete_db(dbname)
 
 if __name__ == '__main__':
     test()
